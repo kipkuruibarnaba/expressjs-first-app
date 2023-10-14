41031cd23d922f6ba6e3395c38bdc0282584a127
Kipkirui@123 <kmutai172@gmail.com>
2023-10-14 17:28:29 +0300

    Updates on pug

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(shopRoutes);
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     // res.status(404).send("<h1>Page Not Found</h1>");
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ router.get('/add-product', (req, res, next) => {
     // app.use('/add-product', (req, res, next) => {
     // res.send();
     //  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product =>POST
